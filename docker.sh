af7690d8ba3a2644fce3012217efe87f0ddc3004
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-12 09:08:23 -0500

    script to trigger docker from scratch

--- docker.sh
+++ docker.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+### BEFORE RUNNING THIS SCRIPT MAKE SURE TO COMMENT THE BELOW UNNECESSARY PARTS
+### RUNNING THIS SCRIPT AS IT IS WILL DELETE ALL YOUR DOCKER CONTAINERS INCLUDING THE DOCKER IMAGES
+
 # stops all existing docker containers
-echo stopping containers
+echo -e '\n**********************************************************************************************'
+echo Stopping Containers
+echo -e '**********************************************************************************************\n'
 docker stop $(docker ps -a -q)
 
 # deletes all existing docker containers
-echo deleting containers
+echo -e '\n**********************************************************************************************'
+echo Deleting Idle Containers
+echo -e '**********************************************************************************************\n'
 docker rm $(docker ps -a -q)
 
 # force deletes all existing docker images
-echo force deleting docker images
+echo -e '\n**********************************************************************************************'
+echo Executing Force Deletion of Docker Images
+echo -e '**********************************************************************************************\n'
 docker rmi $(docker images -q) -f
 
 # docker images
 
 # builds docker image robinhood with tag 1.0
-echo bulding docker image
+echo -e '\n**********************************************************************************************'
+echo Bulding New Docker Image
+echo -e '**********************************************************************************************\n'
 docker build -t robinhood:1.0 .
 
 # runs the docker image
-echo running docker
+echo -e '\n**********************************************************************************************'
+echo Running Docker Image robinhood:1.0
+echo -e '**********************************************************************************************\n'
 docker run robinhood:1.0
